8abe05186e49e6b94d86180fac348e7deac6b949
maslina524 <luk.kirill2804@gmail.com>
2026-05-12 22:10:16 +0300

    added stdout json && only_result flags to clap

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,15 @@ struct Cli {
     #[arg(short = 'f', long = "file")]
     file: String,
 
+    #[arg(short = 's')]
+    stdout: bool, // Outputs everything to stdout
+
+    #[arg(short = 'j')]
+    json: bool, // Outputs everything to stdout
+
+    #[arg(short = 'r', long = "only-result")]
+    only_result: bool, // Outputs only the result of execution
+
     #[command(subcommand)]
     command: Option<Commands>,
 }
