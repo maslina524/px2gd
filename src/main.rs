5726b71ab94bf68a2f07a83e2f1fa4b5e86ae0be
maslina524 <luk.kirill2804@gmail.com>
2026-05-12 21:55:21 +0300

    added clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(name = "px2gd")]
+#[command(about = "A convenient CLI tool for generating raster images in Geometry Dash", long_about = None)]
+#[command(disable_help_subcommand = true)]
+#[command(subcommand_required = false)]
+struct Cli {
+    #[arg(short = 'f', long = "file")]
+    file: String,
+
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands { }
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+    
+    let cmd = match cli.command {
+        Some(c) => {}
+        None => {}
+    };
 }
