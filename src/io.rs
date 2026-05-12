01209180f78d4c99a6e90540b40c05b2a9b24e5f
maslina524 <luk.kirill2804@gmail.com>
2026-05-12 22:44:13 +0300

    added print_result to io.rs

--- src/io.rs
+++ src/io.rs
@@ -1,5 +1,33 @@
+use std::fmt::Display;
+use serde_json::json;
+
 pub struct IOFlags {
     pub stdout: bool,
     pub json: bool,
     pub only_result: bool
+}
+
+pub fn print_result<T, E>(result: Result<T, E>, flags: &IOFlags)
+where
+    T: Display,
+    E: Display,
+{
+    let output = if flags.json {
+        let (status, message) = match &result {
+            Ok(t) => ("Ok", format!("{t}")),
+            Err(e) => ("Err", format!("{e}")),
+        };
+        json!({ "status": status, "message": message }).to_string()
+    } else {
+        match &result {
+            Ok(t) => format!("Ok: {t}"),
+            Err(e) => format!("Err: {e}"),
+        }
+    };
+
+    if result.is_ok() || flags.stdout {
+        println!("{output}")
+    } else {
+        eprintln!("{output}")
+    }
 }
